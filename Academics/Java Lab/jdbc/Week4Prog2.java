2cae47216485f372fbd74e8b6aab93f6330aec42
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2022-11-07 22:36:12 +0530

    Add files via upload

---
+++ Academics/Java Lab/jdbc/Week4Prog2.java
@@ -0,0 +1,103 @@
+package hpack;
+
+import java.util.Scanner;
+import java.sql.*;
+public class Week4Prog2{
+	static void print(ResultSet rs) {
+		try {
+			String f[]=new String[4];
+			while(rs.next()) {
+				for(int i=0;i<4;i++)
+					f[i]=rs.getString(i+1);
+				System.out.println(f[0]+"\t"+f[1]+"\t"+f[2]+"\t"+f[3]);
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	static void modify1(String query,Connection con,String info[]) {
+		PreparedStatement ps=null;
+		try {
+		ps=con.prepareStatement(query);
+		ps.setInt(1,Integer.parseInt(info[0]));
+		ps.setString(2,info[1]);
+		ps.setString(3,info[2]);
+		ps.setDouble(4, Double.parseDouble(info[3]));
+		ps.executeUpdate();
+		Savepoint sp=con.setSavepoint();
+		con.rollback(sp);
+		con.releaseSavepoint(sp);
+		con.commit();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			try {
+			con.rollback();}
+			catch(SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		finally {
+			try {
+				ps.close();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("MySQL Connect Example.");
+		Connection conn = null;
+		String url = "jdbc:mysql://localhost:3306/";
+		String dbName = "Bank_Account";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root"; 
+		String password = "";
+		try {
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+			conn.setAutoCommit(false);
+			System.out.println("Connected to the database");
+			Statement stmt = conn.createStatement();
+			String query="insert into bank (Account_No,Account_Name,Type_of_Account,Balance) values (?,?,?,?)";
+			String info[]=new String[4];
+			System.out.println("Enter Account_No,Account_Name,Type_of_Account,Balance");
+			info[0]=Integer.toString(in.nextInt());
+			in.nextLine();
+			info[1]=in.nextLine();
+			info[2]=in.nextLine();
+			info[3]=Double.toString(in.nextDouble());
+			modify1(query,conn,info);
+			String query1="Select * from bank";
+			ResultSet rs = stmt.executeQuery(query1);
+			print(rs);
+			
+			conn.close();
+			System.out.println("Disconnected from database");
+			} //end try
+
+			catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+
+			catch(SQLException e) {
+			e.printStackTrace();
+			}
+			
+			catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
