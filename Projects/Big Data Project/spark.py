79e3cb17616ed74aa908199ef93f3ca923951abd
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-01-18 12:03:10 +0530

    Delete Projects/Big Data Project directory

--- Projects/Big Data Project/spark.py
+++
@@ -1,7 +0,0 @@
-import os
-from pyspark.sql import SparkSession
-os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 pyspark-shell"
-spark = SparkSession.builder.getOrCreate()
-spark.conf.set(r"spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog")
-spark.read.table("mycatalog.chat_keyspace.chat_table").show()
-spark.stop()
