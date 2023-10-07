65f5de85f63354eecc13073fa8c37f86a1591933
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-10-07 18:20:47 +0530

    Update insert.php

--- Projects/phpproject/insert.php
+++ Projects/phpproject/insert.php
@@ -9,12 +9,7 @@
 <p align="right"><a href='index.php'>Back to Homepage</a></p>
 	<center>
 		<?php
-
-		// servername => localhost
-		// username => root
-		// password => empty
-		// database name => staff
-		$conn = mysqli_connect("localhost", "id19616248_root", "1+cj>I~[Mb]RK5x$", "id19616248_staff");
+		$conn = mysqli_connect("localhost", "root", "", "staff");
 		
 		// Check connection
 		if($conn === false){
@@ -22,28 +17,42 @@
 				. mysqli_connect_error());
 		}
 		
-		// Taking all 5 values from the form data(input)
 		$first_name = $_REQUEST['first_name'];
 		$last_name = $_REQUEST['last_name'];
 		$gender = $_REQUEST['gender'];
-		$city_village_town = $_REQUEST['city/village/town'];
+		$city = $_REQUEST['city'];
 		$district = $_REQUEST['district'];
 		$state = $_REQUEST['state'];
 		$email = $_REQUEST['email'];
-                $phone = $_REQUEST['phone_no'];
-		$crop= $_REQUEST['exam'];
+        $phone = $_REQUEST['phone_no'];
+		$crop='';
+		$x=$_REQUEST['croptype'];
+		for($i=0;$i<count($x);$i++){
+			$crop.=$x[$i].',';
+			if($x[$i]=='1')
+			$crop.= implode(',',$_REQUEST['fruits']);
+			elseif($x[$i]=='2')
+			$crop.= implode(',',$_REQUEST['vegetables']);
+			elseif($x[$i]=='3')
+			$crop.= implode(',',$_REQUEST['pulses']);
+			elseif($x[$i]=='4')
+			$crop.= implode(',',$_REQUEST['grains']);
+			if($i!=count($x)-1)
+			$crop.=',';
+		}
 		
 		// Performing insert query execution
 		// here our table name is college
+		
 		$sql = "INSERT INTO college VALUES ('$first_name',
-			'$last_name','$gender','$city_village_town','$district','$state','$email','$phone','$crop')";
+			'$last_name','$gender','$city','$district','$state','$email','$phone','$crop')";
 		
 		if(mysqli_query($conn, $sql)){
 			echo "<h3>Success!"
 				. " Data stored in the database successfully.</h3>";
 
 			echo nl2br("\nFirst Name: $first_name\n Last Name: $last_name\n "
-				. "Gender: $gender\nCity/Village/Town: $city_village_town\nDistrict: $district\nState: $state\nAadhar: $email\nPhone: $phone\nCrop: $crop");
+				. "Gender: $gender\nCity/Village/Town: $city\nDistrict: $district\nState: $state\nEmail: $email\nPhone: $phone\nCrop: $crop");
 		} else{
 			echo "ERROR:Sorry, data was not stored $sql. "
 				. mysqli_error($conn);
